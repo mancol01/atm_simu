9f89cd1ce1680499d657e2138627ca4d465e8901
mancol01 <mankcolly321@gmail.com>
2021-08-16 19:35:26 +0200

    update version +1
    added new feture, restructured database, update to 1.1.0

--- ATM.py
+++ ATM.py
@@ -5,34 +5,195 @@ Description: atm simulation
 '''
 import time
 
-div='-'*50
-div2='_'*50
+div='-'*58
+div2='_'*58
 print(div)
 accept= ['y','yes','Yes','Y','YES']
 reject= ['n','no','No','N','No']
 dbpin='1234'
 chances=3
+#######################################
 
 
-try:
-    b=open('bal.txt')
+###########
+#reading from file
+filBal=open('dbal2.txt')
+db=open('clientDB.txt')
+fLis=[]
+account=input('Enter your account number:\n')
+
+#intialising a list
+balList=[]
+
+for line in filBal:
+    dat=line.strip()
+    balList.append(dat)
+
+a=balList[0]
+b=balList[1]
+c=a.split('_')
+d=b.split('_')
+
+
+balDict=dict(zip(c,d))
+
+###################
+#getting balance from dbBal
+def getBal(*args):
+    global bal
+    for i in balDict:
+        if account==i:
+            bal=balDict[i]
+    return bal
     
-except:
-    b=open('bal.txt','w')
-    b.write('20')
-finally:
-    b=open('bal.txt','r+')
+    
+getBal(balDict)        
+#print(bal)
+
+#returning balance
+def retBal(*args):
+    cont1=[]
+    cont2=[]
+    balax = bal
+    try:
         
+        for i in balDict:
+            if account==i:
+                #print(i)
+                balDict[account]=str(balax)
+                
+    except RuntimeError:
+                    
+        print('Working')
+        #print(balDict)
+        
+    #print(balDict)
+    for i in balDict.items():
+        f=list(i)
+        #print(f)
+        a=f[0]
+        b=f[1]
+        #print(a)
+        #print(b)
+        cont1.append(a)
+        cont2.append(b)
+        #print(cont1)
+        #print(cont2)
+        w='_'.join(cont1)
+        x='_'.join(cont2)
+        #print(w)
+        #print(x)
+        
+        with open('dbal2.txt', 'w+') as file:
+            file.write(f"{w}\n{x}")
+            
+        
+
+
+###################
+###################
+
+
+for line in db:
+    lin=line.strip()
+    fLis.append(lin)
     
-for line in b:
-    bal=line.strip()
+
+lnA=fLis[0]
+lnB=fLis[1]
+lnC=fLis[2]
+
+
+LA=lnA.split('_')
+LB=lnB.split(',')
+
+
+SLC=lnC.split('_')
+nbl=[]
+
+for e in SLC:
+    LC=e.split(',')
+    nbl.append(LC)    
+
+
+listOfD=[]
+for i in nbl:
+    anot_CL=dict(zip(LB,i))
+    listOfD.append(anot_CL)
+
+
+fDict=dict(zip(LA,listOfD))
+
+#print(fDict)
+#to process data in the clientDB atfer generating fDict
+def item_ret(*args):
+    global firstName, surName, regNumberID, emailAddress, cellNumber, occupation, natIDNum, DateOfBirth, accountType, RegDate, sex, title
+    for key in fDict:
+        #print(key)
+        if account not in fDict:
+            break
+        elif key == account:
+            v=fDict[key]
+            nested_dict=v
+            for ndkey in nested_dict:
+                if ndkey=='fName':
+                    firstName=nested_dict[ndkey]
+                elif ndkey=='sName':
+                    surName=nested_dict[ndkey]
+                elif ndkey=='reg.I.D':
+                    regNumberID=nested_dict[ndkey]
+                    
+                elif ndkey=='email-address':
+                    emailAddress=nested_dict[ndkey]
+                elif ndkey=='cell-number':
+                    cellNumber=nested_dict[ndkey]
+                elif ndkey=='occupation':
+                    occupation=nested_dict[ndkey]
+                elif ndkey=='nat-I.D.-or-Passport-Num':
+                    natIDNum=nested_dict[ndkey]
+                elif ndkey=='D.O.B':
+                    DateOfBirth=nested_dict[ndkey]
+                elif ndkey=='account-type':
+                    accountType=nested_dict[ndkey]
+                elif ndkey=='Reg-Date':
+                    RegDate=nested_dict[ndkey]
+                elif ndkey=='sex':
+                    sex=nested_dict[ndkey]
+                elif ndkey=='title':
+                    title=nested_dict[ndkey]
+                
+                
+    try:
+        
+        return firstName, surName, regNumberID, emailAddress, cellNumber, occupation, natIDNum, DateOfBirth, accountType, RegDate, sex, title
+    except:
+        print("account not found")
+    
+
+    
+item_ret(fDict, account)
+try:
+    print(f"\aACCOUNT TYPE:  >_\t{accountType}")
+except NameError:
+    print('balanc could no be retrieved')
+    
+    
+print(f"ACCOUNT HOLDER:>_\t{title} {firstName} {surName}\nACCOUNT NUMBER:>_\t{account} ")
+T=[firstName, surName, regNumberID, emailAddress, cellNumber, occupation, natIDNum, DateOfBirth, accountType, RegDate, sex, title]
+#print(T)
+
+#######################################
          
 
 bal=float(bal)
 
 
 def greet():
-    print(f'\n{div}\nGreetings\nWelcome to Mancol Financial House(MFH) Pvt Ltd\n{div}')
+    ltrs=[]
+    for i in firstName:
+        ltrs.append(i)
+        fc=ltrs[0]
+    print(f'\n[{div}]\n\tGreetings \t{title} {fc} {surName} \n\tWelcome to Mancol Financial House(MFH) Pvt Ltd\n[{div}]')
     
     
     
@@ -46,7 +207,7 @@ def tryagain():
             
 def option():
     global opt
-    opt=input('Select an option:\n\tWithdraw money press [1]\n\tdeposit cash press [2]\n\tTo check balance [3]\n\t>_ ')
+    opt=input('Select an option:\n\tWithdraw money press [1]\n\tDeposit cash press [2]\n\tTo check balance [3]\n\t>_ ')
     atm(opt)
     return opt
     
@@ -55,7 +216,7 @@ def pintrial(chances):
     if chances>0:
         security(chances)
     else:
-        print(f"{div}\nNo more chances, your account is suspended!\n{div}")
+        print(f"{div2}\nNo more chances, your account is suspended!\n{div2}")
         outro()
     pass
 
@@ -76,7 +237,7 @@ def security(chances):
         
 
 def again():
-    agn=input("Do you want to transact again? [y] or [n]")
+    agn=input("Do you want to transact again? [y] or [n]:\t")
     if agn in accept:
         option()
     if agn in reject:
@@ -97,10 +258,14 @@ def withdraw(*args):
         else:
             bal-=amt
             print("Successfully withdrawn :-)\nYour current balance is ${0}".format(bal))
+            retBal(balDict, bal)
+            '''
             b.seek(0)
             b.truncate()
             print(bal)
             b.write(str(bal))
+            '''
+            
             again()
     
     
@@ -108,7 +273,7 @@ def withdraw(*args):
 
 def deposit(*args):
     global bal
-    print(div)
+    print(div2)
     print('\n\tMaximum deposit per day is $500.00\n')
     print(div)
     dep=input('Enter the amount you want to deposit:\n')
@@ -119,10 +284,13 @@ def deposit(*args):
     else:
             bal+=dep
             print("Successfully deposited :-)\nYour current balance is ${0}".format(bal))
+            retBal(balDict, bal)
+            '''
             b.seek(0)
             b.truncate()
             print(bal)
             b.write(str(bal))
+            '''
             again()
 
 
@@ -147,7 +315,7 @@ def balance(*args):
 
 
 def outro():
-    print(f"Thank you for doing business with us\nGoodbye!\n{div}")
+    print(f"Thank you {title} {surName} for doing business with us\nGoodbye!\n[{div}]")
     time.sleep(1)
     return exit()
 
@@ -168,5 +336,6 @@ def atm(opt):
         
 greet()
 security(chances)
+
 
 
--- README.md
+++ README.md
@@ -1,17 +1,51 @@
 # ATM_Simu_version_1.0.0
 ## This is an atm simulation program written in python
-### Features of ATM_Simu_version_1.0.0
+## Details about the author:
+<pre>
+    Name: Mandla Collymore Mangena
+    State: Zimbabwe
+    City: Harare
+    email: mandlcollmang321@gmail.com
+    cell: +263 784437655/8
+</pre>
+
+### Features of ATM_Simu_version_1.0.0 (15-08-2021)
 <p>
-  <ul>
-    <h5>Personalisation</h5>
-    <li>Greets</li>
-    <li>Gives information on the institution</li>
-    <li>Gives a farewell</li>
-    <h5>Data handling</h5>
-    <li>Reads balance from external file and updates it before closing</li>
+    <ul>
+        <h5>Personalisation</h5>
+            <li>Greets</li>
+            <li>Gives information on the institution</li>
+            <li>Gives a farewell</li>
+        <h5>Data handling</h5>
+            <li>Reads balance from external file and updates it before closing</li>
     <h5>Main features</h5>
-    <ol>
-    </ol>
-  </u>
+        <ol>
+            <li>Security</li>
+            <li>Withdraw option</li>
+            <li>Deposit option</li>
+            <li>Check balance</li>
+        </ol>
+    </u>
+</p>
+<hr/>
+
+### Features of ATM_Simu_version_1.1.0 (16-08-2021)
+<p>
+    <ul>
+        <h5>Personalisation</h5>
+            <li>Greets with client information</li>
+            <li>Gives information on the institution</li>
+            <li>Gives a farewell</li>
+        <h5>Data handling</h5>
+            <li>Reads client data from a txt file as database 
+            <li> Reads a separate file that contain acc nums and balances updates it before closing</li>
+    <h5>Main features</h5>
+        <ol>
+            <li>Security</li>
+            <li>Withdraw option</li>
+            <li>Deposit option</li>
+            <li>Check balance</li>
+        </ol>
+    </u>
 </p>
   
